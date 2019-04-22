6bb0e30ec7c2a352f8345f324f3da6a9db607520
Edgar Irizarry <eirizarry15@yahoo.com>
2019-04-22 14:45:49 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,11 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    query = params[:query]
+    @res = Faraday.get "https://api.github.com/search/repositories" do |req|
+      req.params['q'] = query
+    end
+    @repos = JSON.parse(@res.body)["items"]
+    render 'search'
   end
 end
